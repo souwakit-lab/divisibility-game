ba0e8738c2f241d7c03b27b6de29d18084e80fa8
Keith Sou <keithsouai@gmail.com>
2026-09-22 08:44:03 +0800

    Connect classroom data service

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.DIVISIBILITY_CONFIG = {
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxSTZ1HgFexPUhqhPJxwvVynAHVTgkenz99--21PEi2GKPs6beia2TMgAY3KirvSlQgnA/exec",
   studentUrl: "https://souwakit-lab.github.io/divisibility-game/",
 };
